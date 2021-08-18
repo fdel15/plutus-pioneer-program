31a84547452e9690ab139d88e59ff6585eebae6b
fdel15 <frankdelpidio@gmail.com>
2021-08-18 04:25:41 -0400

    Complete homework

--- code/week09/app/marlowe.hs
+++ code/week09/app/marlowe.hs
@@ -10,55 +10,85 @@ choiceId p = ChoiceId "Winner" p
 contract :: Party -> Party -> Party -> Value -> Contract
 contract alice bob charlie deposit =
     When
-        [ f alice bob
-        , f bob alice
+        [ f alice bob charlie
+        , f bob alice charlie
         ]
         10 Close
   where
-    f :: Party -> Party -> Case
-    f x y =
+    f :: Party -> Party -> Party -> Case
+    f x y z =
         Case
+            -- Action
             (Deposit
                 x
                 x
                 ada
                 deposit
             )
+            -- Contract 1
             (When
                 [Case
+                    -- Action
                     (Deposit
                         y
                         y
                         ada
                         deposit
                     )
+                    -- Contract 2
                     (When
                         [Case
-                            (Choice
-                                (choiceId charlie)
-                                [Bound 1 2]
+                            -- Action
+                            (Deposit
+                                z
+                                z
+                                ada
+                                (MulValue deposit (Constant 2))
                             )
-                            (If
-                                (ValueEQ
-                                    (ChoiceValue $ choiceId charlie)
-                                    (Constant 1)
-                                )
-                                (Pay
-                                    bob
-                                    (Account alice)
-                                    ada
-                                    deposit
-                                    Close
-                                )
-                                (Pay
-                                    alice
-                                    (Account bob)
-                                    ada
-                                    deposit
-                                    Close
-                                )
+                            -- Contract 3
+                            (When
+                                [Case
+                                    -- Action
+                                    (Choice
+                                        (choiceId charlie)
+                                        [Bound 1 2]
+                                    )
+                                    -- Contract 4
+                                    (If
+                                        (ValueEQ
+                                            (ChoiceValue $ choiceId charlie)
+                                            (Constant 1)
+                                        )
+                                            (Pay
+                                                bob
+                                                (Account alice)
+                                                ada
+                                                deposit
+                                                Close -- Ends Contract 4
+                                            )
+                                            (Pay
+                                                alice
+                                                (Account bob)
+                                                ada
+                                                deposit
+                                                Close -- Ends Contract 4
+                                            )
+                                    )]
+                            30 (Pay
+                                  charlie
+                                  (Account bob)
+                                  ada
+                                  deposit
+                                  (Pay
+                                     charlie
+                                     (Account alice)
+                                     ada
+                                     deposit
+                                     Close
+                                  )
+                                ) -- Ends Contract 3
                             )]
-                        30 Close
+                        20 Close -- Ends Contract 2
                     )]
-                20 Close
+                20 Close -- Ends Conract 1
             )
